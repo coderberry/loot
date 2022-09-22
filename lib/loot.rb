3136a0c2517c3b7fffa7f51e632f32eb1bfd6b01
Eric Berry <eric@berry.sh>
2022-09-22 17:00:53 -0600

    Initial commit

---
+++ lib/loot.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "loot/version"
+
+module Loot
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/loot_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Loot do
+  it "has a version number" do
+    expect(Loot::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
